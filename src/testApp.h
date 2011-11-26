f50f6659bef8620a4cb7759cc91bc00da8186e1d
Andrew Sliwinski <thisandagain@gmail.com>
2011-11-25 17:34:56 -0800

    Migrated to audioRequested event

--- src/testApp.h
+++ src/testApp.h
@@ -28,22 +28,28 @@ class testApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+		void keyPressed         (int key);
+		void keyReleased        (int key);
+		void mouseMoved         (int x, int y );
+		void mouseDragged       (int x, int y, int button);
+		void mousePressed       (int x, int y, int button);
+		void mouseReleased      (int x, int y, int button);
+		void windowResized      (int w, int h);
+		void dragEvent          (ofDragInfo dragInfo);
+		void gotMessage         (ofMessage msg);
 		
 		// Define functions
-        void audioGUI(int x, int y, int guiw, int nChannels);
-		void audioOut(float * input, int bufferSize, int nChannels);
+        void audioGUI           (int x, int y, int guiw, int nChannels);
+		void audioRequested     (float * input, int bufferSize, int nChannels);
 		
 		// Sound stream
 		ofSoundStream soundStream;
+    
+        // Sample
+        int     bufferSamples;
+        vector  <MiniMaxima> waveForm;
+        Sample  sample;
+        float   deltax;
 
 		// Trackers
 		float   xaxis;
@@ -55,15 +61,9 @@ class testApp : public ofBaseApp{
 		int		sampleRate;
 		bool 	bNoise;
 		float 	volume;
-	
+    
 		// Output channels (# of channels + 1)
         vector <float> channel[9];
-		
-		// Sine wave synthesis
-		float 	targetFrequency;
-		float 	phase;
-		float 	phaseAdder;
-		float 	phaseAdderTarget;
 };
 
 #endif
